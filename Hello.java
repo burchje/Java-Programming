3b6922f86ea82c949d4e1d0b17f9998a73d5e155
burchje <59924635+burchje@users.noreply.github.com>
2020-01-15 10:13:49 -0500

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -6,5 +6,13 @@ public class Hello {
 		System.out.println("01/15/2020");
 		System.out.println("CMNC255 003");
 		System.out.println();
+		System.out.println( _______   __                         __);
+		System.out.println(| ______| |  |  ___     ___   ____   |  |);
+		System.out.println(| |__     |  | |___|   |___| / ___\  |  |);
+		System.out.println(|  __|    |  |  ___     ___  |/  \|  |  |__);
+		System.out.println(| |_____  |  | |   |   |   | \\__/|  |  __ \;
+		System.out.println(|_______| |__| |___|   |   |    |_|  |_/  \_|);
+		System.out.println(                    ___/   /);
+		System.out.println(                    \____/);
 	}
 }
