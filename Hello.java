a20326aff3a4fe54f0c83c2efe2666f5e342a524
burchje <59924635+burchje@users.noreply.github.com>
2020-01-15 09:55:54 -0500

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Elijah Burch");
 		System.out.println("Hello.java");
-		System.out.println("The goal of this lab is to first make sure that you have Java and all related applications installed and ready to go. Secondly, the goal is for you to make sure that you can run your Java program on the computers in the lab.");
+		System.out.println("Project 1");
 		System.out.println("01/15/2020");
 		System.out.println("CMNC255 003");
 		System.out.println();
