0f2f5c8f22102a51754e9c8ce375c295f4592bc3
burchje <59924635+burchje@users.noreply.github.com>
2020-01-16 09:31:51 -0500

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,18 +1,19 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Elijah Burch");
-		System.out.println("Hello.java");
 		System.out.println("Project 1");
+		System.out.println("This project outputs my name in a creative and unique way");
 		System.out.println("01/15/2020");
-		System.out.println("CMNC255 003");
+		System.out.println("CMNC 255 003");
 		System.out.println();
-		System.out.println( _______   __                         __);
-		System.out.println(| ______| |  |  ___     ___   ____   |  |);
-		System.out.println(| |__     |  | |___|   |___| / ___\  |  |);
-		System.out.println(|  __|    |  |  ___     ___  |/  \|  |  |__);
-		System.out.println(| |_____  |  | |   |   |   | \\__/|  |  __ \);
-		System.out.println(|_______| |__| |___|   |   |    |_|  |_/  \_|);
-		System.out.println(                    ___/   /);
-		System.out.println(                    \____/);
+		System.out.println(" *******   **                         **");
+		System.out.println(" **        **   ***     ***    ***    **");
+		System.out.println(" ****      **   ***     ***   *  *    **");
+		System.out.println(" ****      **                 *  *    * **");
+		System.out.println(" **        **   ***     ***    ***    *   *");
+		System.out.println(" *******   **   ***     ***           *   *");
+		System.out.println("                    *  ***");
+		System.out.println("                     ****");
+		System.out.println();
 	}
 }
