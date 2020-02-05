c0facc7cde4de41fb4ada7fc4e21051b3d24d057
Elijah Burch <59924635+burchje@users.noreply.github.com>
2020-02-05 10:44:18 -0500

    Add files via upload

--- PhoneSelection.java
+++ PhoneSelection.java
@@ -1,5 +1,11 @@
 /******************
-
+PhoneSelection
+This program takes a single letter input and displays the
+corresponding digit on the telephone.
+Elijah Burch - Yash Upadhyay - Harrison Brock
+2/5/2020
+CMNC 255 003
+******************/
 
 import java.util.Scanner;
 public class PhoneSelection {
@@ -26,30 +32,30 @@ public class PhoneSelection {
 		switch (letter){
 			case 'A':
 			case 'B':
-			case 'C': System.out.println("The digit 2 corresponds to " + letter + " on the telephone."); break;
+			case 'C': System.out.println("The digit 2 corresponds to the letter " + letter + " on the telephone."); break;
 			case 'D':
 			case 'E':
-			case 'F': System.out.println("The digit 3 corresponds to " + letter + " on the telephone."); break;
+			case 'F': System.out.println("The digit 3 corresponds to the letter " + letter + " on the telephone."); break;
 			case 'G':
 			case 'H':
-			case 'I': System.out.println("The digit 4 corresponds to " + letter + " on the telephone."); break;
+			case 'I': System.out.println("The digit 4 corresponds to the letter " + letter + " on the telephone."); break;
 			case 'J':
 			case 'K':
-			case 'L': System.out.println("The digit 5 corresponds to " + letter + " on the telephone."); break;
+			case 'L': System.out.println("The digit 5 corresponds to the letter " + letter + " on the telephone."); break;
 			case 'M':
 			case 'N':
-			case 'O': System.out.println("The digit 6 corresponds to " + letter + " on the telephone."); break;
+			case 'O': System.out.println("The digit 6 corresponds to the letter " + letter + " on the telephone."); break;
 			case 'P':
 			case 'Q':
 			case 'R':
-			case 'S': System.out.println("The digit 7 corresponds to " + letter + " on the telephone."); break;
+			case 'S': System.out.println("The digit 7 corresponds to the letter " + letter + " on the telephone."); break;
 			case 'T':
 			case 'U':
-			case 'V': System.out.println("The digit 8 corresponds to " + letter + " on the telephone."); break;
+			case 'V': System.out.println("The digit 8 corresponds to the letter " + letter + " on the telephone."); break;
 			case 'W':
 			case 'X':
 			case 'Y':
-			case 'Z': System.out.println("The digit 9 corresponds to " + letter + " on the telephone."); break;
+			case 'Z': System.out.println("The digit 9 corresponds to the letter " + letter + " on the telephone."); break;
 			/************
 			* Make sure ‘letter’ is a single letter and not a number or word
 			************/
